2101fbafd2e47a2a0d25f0224127674f1c0d6790
Aaryan Khedekar <aaryankhclasses07@gmail.com>
2021-05-16 14:21:36 +0530

    heroku :/ v2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const loadCommands = require('./commands/load-commands.js')
 const levelling = require('./utils/levelling.js')
 const levels = require('./utils/levels.js')
 const loadEvents = require('./events/load-events.js')
-const loadFeatures = require('./features/load-features.js')
+// const loadFeatures = require('./features/load-features.js')
 
 client.on('ready', () => {
     console.log('The Bot is Online!')
