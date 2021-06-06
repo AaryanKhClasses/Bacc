1ff73c9571b02d02df9fdebf35ffdf84b8de3567
Aaryan Khedekar <aaryankhclasses07@gmail.com>
2021-06-06 13:02:53 +0530

    8ball and ddg cmds

---
+++ commands/commands/fun/8ball.js
@@ -0,0 +1,32 @@
+const config = require('../../../config.json')
+const { MessageEmbed } = require('discord.js')
+const { replies } = require('../../../json/8ball.json')
+
+module.exports = {
+    commands: '8ball',
+    cooldown: 10,
+    description: 'Get the answer to all your questions here :D',
+    usage: '!8ball',
+    permLevel: 0,
+    callback: (client, message, args) => {
+        const query = args.join(' ')
+        if(!query) {
+            const embed = new MessageEmbed()
+            .setColor('RED')
+            .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL())
+            .setFooter(config.botname)
+            .setTimestamp()
+            .setDescription(`${config.emojis.no} Please ask your question so that I can answer it!`)
+            message.lineReply(embed)
+        }
+
+        const result = Math.floor((Math.random() * replies.length))
+        const embed = new MessageEmbed()
+        .setColor('GREEN')
+        .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL())
+        .setFooter(config.botname)
+        .setTimestamp()
+        .setDescription(`${config.emojis.magic} The Magical 8Ball!\n${config.emojis.info} **Question:** \`${query}\`\n${config.emojis.arrowRight} **Answer:** \`${replies[result]}\``)
+        message.lineReply(embed)
+    }
+}
---
+++ commands/commands/fun/ddg.js
@@ -0,0 +1,54 @@
+const config = require('../../../config.json')
+const { MessageEmbed } = require('discord.js')
+const search = require('node-ddg').default
+
+module.exports = {
+    commands: 'ddg',
+    cooldown: 10,
+    description: 'Searches for the specified information on DDG',
+    usage: '!ddg [query]',
+    permLevel: 0,
+    callback: (client, message, args) => {
+        const query = args.join(' ')
+        if(!query) {
+            const embed = new MessageEmbed()
+            .setColor('RED')
+            .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL())
+            .setFooter(config.botname)
+            .setTimestamp()
+            .setDescription(`${config.emojis.no} Please specify a query to search on DDG!`)
+            message.lineReply(embed)
+        }
+
+        let options = {
+            count: 0,
+            offset: 0,
+            lang: 'en-US,en;q=0.9',
+            debug: false,
+            show: false,
+            screenshot: false,
+            wait: 0
+        }
+
+        let string = ''
+        let embed = new MessageEmbed()
+
+        search({ query: query, maxResults: 1 }).then((results) => {
+            for(let i = 0; i < 10; i++) {
+                if(i > results.length - 1) {
+                    break
+                }
+
+                string = string + `\n**${results[i].title}**\n${results[i].body}\n${results[i].url}`
+
+                embed
+                .setColor('GREEN')
+                .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL())
+                .setFooter(config.botname)
+                .setTimestamp()
+                .setDescription(`${string}`)
+            }
+            message.lineReply(embed)
+        })
+    }
+}
