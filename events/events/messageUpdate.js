f2d8c5fc7ac88a75f5571b2b15e7b689adb718ce
Cobweb Dev <71999854+SpiderMath@users.noreply.github.com>
2021-07-10 13:41:02 +0530

    Fixed the Message No Edit Bug
    People often open the message editing box on Discord by mistake, and when they exit, the event is fired. But it doesn't always lead to change in content or anything. So the bot often logs unnecessary stuff... 
    This commit fixes that!

--- events/events/messageUpdate.js
+++ events/events/messageUpdate.js
@@ -3,7 +3,8 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = (client) => {
     client.on('messageUpdate', (oldMessage, newMessage) => {
-        if(oldMessage.author.bot) return
+        if(oldMessage.author.bot) return;
+        if(oldMessage.content === newMessage.content) return;
         const channell = oldMessage.guild.channels.cache.find(ch => ch.name.includes("mod-logs")).id
 
         const logEmbed = new MessageEmbed()
