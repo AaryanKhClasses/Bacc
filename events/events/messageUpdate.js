335da98eace94e63c9c94b65e7cafbf0307e0578
Cobweb Dev <71999854+SpiderMath@users.noreply.github.com>
2021-07-10 14:12:54 +0530

    Updated in V12 Bronch

--- events/events/messageUpdate.js
+++ events/events/messageUpdate.js
@@ -3,8 +3,9 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = (client) => {
     client.on('messageUpdate', (oldMessage, newMessage) => {
-        if(oldMessage.author.bot) return
-        const channell = oldMessage.guild.channels.cache.find(ch => ch.name.includes("mod-logs")).id
+        if(oldMessage.author.bot) return;
+        if(oldMessage.content === newMessage.content) return;
+        const channell = oldMessage.guild.channels.cache.find(ch => ch.name.includes("mod-logs")).id;
 
         const logEmbed = new MessageEmbed()
         .setColor('BLUE')
@@ -14,7 +15,7 @@ module.exports = (client) => {
         .addField(`${config.emojis.user} Message Author`, `<@${oldMessage.author.id}> (${oldMessage.author.id})`)
         .addField(`${config.emojis.topic} Channel`, `<#${oldMessage.channel.id}> (${oldMessage.channel.id})`)
         .addField(`${config.emojis.text} Old Message`, oldMessage)
-        .addField(`${config.emojis.arrowRight} New Message`, newMessage)
-        oldMessage.guild.channels.cache.get(channell).send(logEmbed)
+        .addField(`${config.emojis.arrowRight} New Message`, newMessage);
+        oldMessage.guild.channels.cache.get(channell).send(logEmbed);
     })
 }
